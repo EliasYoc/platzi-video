11a99764a97a8780fa142cd79d629d9e672b62c7
EliasYoc <eliyoc_97@hotmail.com>
2021-10-01 18:03:36 -0500

    primer avance carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Header from "./componentes/Header";
 import "./assets/styles/App.scss";
+import Search from "./componentes/Search";
+import Categories from "./componentes/Categories";
+import Carousel from "./componentes/Carousel";
+import CarouselItem from "./componentes/CarouselItem";
+import Footer from "./componentes/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
     </div>
   );
 }
---
+++ src/componentes/Carousel.jsx
@@ -0,0 +1,10 @@
+import "../assets/styles/components/Carousel.scss";
+const Carousel = ({ children }) => {
+  return (
+    <section className="carousel">
+      <div className="carousel__container">{children}</div>
+    </section>
+  );
+};
+
+export default Carousel;
